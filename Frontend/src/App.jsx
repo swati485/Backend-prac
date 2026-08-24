11eb4a452301d3a60d31258d0e2aff574df1985f
swati485 <swati2520sharma@gmail.com>
2026-08-24 13:57:22 +0530

    Add full stack project

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,62 @@
+import { useState,useEffect } from 'react'
+import axios from "axios"
+
+
+function App() {
+
+  const [notes, setNotes] = useState([])
+
+  function fetchNotes(){
+     axios.get('http://localhost:3000/api/notes')
+      .then((res) => {
+        setNotes(res.data.notes)
+      })
+  }
+
+  useEffect(() => {
+    fetchNotes()
+  }, [])
+
+  function handleSubmit(e){
+    e.preventDefault()
+
+    const {title,description}=e.target.elements
+
+    console.log(title.value,description.value)
+
+    axios.post("http://localhost:3000/api/notes",{
+      title:title.value,
+      description:description.value
+    })
+    .then(res=>{
+      console.log(res.data)
+
+      fetchNotes()
+    }
+  )}
+  
+  return (
+    <>
+  
+    <form className='note-create-form' onSubmit={handleSubmit}>
+      <input name='title' type="text" placeholder='Enter Title' />
+      <input name='description' type="text" placeholder='Enter Description'/>
+      <button>Create Note</button>
+    </form>
+
+      <div className="notes">
+        {
+          notes.map(note => {
+            return <div className="note">
+              <h1>{note.title}</h1>
+              <p>{note.description}</p>
+            </div>
+
+          })
+        }
+      </div>
+    </>
+  )
+}
+
+export default App
