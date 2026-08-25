0bb07ddf7cfa5a2051471f02492d30352ac697f0
swati485 <swati2520sharma@gmail.com>
2026-08-25 14:13:21 +0530

    Deploy frontend with backend

--- notes-manager/Backend/src/app.js
+++ notes-manager/Backend/src/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const noteModel = require("./models/note.model")
 const cors=require("cors")
+const path=require("path")
 
 const app = express()
-
 app.use(express.json())
 app.use(cors())
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.post('/api/notes', async (req, res) => {
     const { title, description } = req.body
@@ -52,6 +53,11 @@ app.patch('/api/notes/:id', async (req, res) => {
         message: "Note updated successfully.",
         note: updatedNote
     })
+})
+
+app.use('/{*name}',(req,res)=>{
+    res.sendFile(path.join(__dirname,"..","public","index.html"))
+
 })
 
 module.exports = app
